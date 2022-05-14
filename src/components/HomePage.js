15626a98162106de67f6651fec9fb918215978e7
balkiss-hue <ogundotunbalkiss@gmail.com>
2022-05-14 07:40:45 +0100

    Homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,25 @@
+
+
+export default function Homepage(){
+    return(
+        <div >
+            <div className="bg-black text-white">
+                <nav className="flex justify-between font-bold max-w-6xl mx-auto py-6">
+                    <div className=" ">
+                        <ul>
+                            <li>MyLocale</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul className="flex justify-between space-x-8">
+                            <li>Home</li>
+                            <li>Users</li>
+                            <li>My Profile</li>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
