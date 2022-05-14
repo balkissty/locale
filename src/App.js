046f900d18fda52cb263b350b67d4096561cb311
balkiss-hue <ogundotunbalkiss@gmail.com>
2022-05-14 14:53:03 +0100

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Homepage from "./components/Navbar";
 import './App.css';
 import Navbar from "./components/Navbar";
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+
+
+export default function Homepage() {
+    return(
+        <div>
+            <div className="max-w-7xl mx-auto py-10">
+                <p className="text-pink-600 text-xl">Hi!</p>
+            </div>
+        </div>
+    )
+}
