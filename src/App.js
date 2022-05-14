6431d78a311e907b6c728c3a5b5a79c551fb9609
balkiss-hue <ogundotunbalkiss@gmail.com>
2022-05-15 00:43:55 +0100

    Profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Homepage from './components/Homepage';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+
+export default function Profile() {
+    return(
+        <div>
+            <div className="max-w-4xl mx-auto">
+                <p className="text-4xl pb-6">
+                    Edit Your Profile
+                </p>
+                <form className="grid">
+                    <input type="text" placeholder="Enter new password" className="py-1 pl-2 w-7/12 border-solid border-2 border-gray-500 rounded" />
+                    <button type="submit" className="mt-5 w-1/12 bg-pink-600 text-white font-medium border-0 p-2 rounded-lg">Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+}
