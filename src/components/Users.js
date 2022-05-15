1bc74ce9adea8da9986a090a12084a8f6820a8a9
balkiss-hue <ogundotunbalkiss@gmail.com>
2022-05-15 22:58:11 +0100

    input type

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,8 +5,8 @@ export default function Users(){
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-end max-w-5xl mx-auto">
                     <form class="flex justify-between border mt-24 text-sm">
-                        <input class="text-gray-400 py-2 text-grey-darkest px-2 text-lg" type="text" placeholder="Search a User" />
-                        <span class="flex items-center bg-grey-100 border-0 font-bold">
+                        <input class="text-gray-400 py-2 px-2 text-lg" type="text" placeholder="Search a User" />
+                        <span class="flex items-center font-bold">
                             <button class="text-lg text-gray-400 bg-gray-200 py-3 px-6">Search</button>
                         </span>
                     </form>
