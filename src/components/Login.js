301cf94a4b238a743660ef8aec058059515042cc
balkiss-hue <ogundotunbalkiss@gmail.com>
2022-05-19 20:24:14 +0100

    styled login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -49,7 +49,7 @@ export default function Login() {
        }) => (
             <form onSubmit={handleSubmit}>
                 <div className="form-group mb-6">
-                    <label for="exampleInputEmail2" className="form-label inline-block mb-2 text-gray-700">Email address</label>
+                    <label for="InputEmail" className="form-label inline-block mb-2 text-gray-700">Email address</label>
                     <input 
                         type="email"
                         name="email"
@@ -70,12 +70,12 @@ export default function Login() {
                         transition
                         ease-in-out
                         m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="inputEmail"
                         aria-describedby="emailHelp" placeholder="Enter email" />
                         {errors.email && touched.email && errors.email}
                 </div>
                 <div className="form-group mb-6">
-                    <label for="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Password</label>
+                    <label for="Password" className="form-label inline-block mb-2 text-gray-700">Password</label>
                     <input
                         type="password"
                         name="password"
@@ -95,7 +95,7 @@ export default function Login() {
                         transition
                         ease-in-out
                         m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputPassword2"
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="inputPassword"
                         placeholder="Password" />
                         {errors.password && touched.password && errors.password}
                 </div>
@@ -103,8 +103,8 @@ export default function Login() {
                     <div className="form-group form-check">
                         <input type="checkbox"
                         className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        id="exampleCheck2" />
-                        <label className="form-check-label inline-block text-gray-800" for="exampleCheck2">Remember me</label>
+                        id="Check" />
+                        <label className="form-check-label inline-block text-gray-800" for="Check">Remember me</label>
                     </div>
                     <a href="#!"
                         className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Forgot
@@ -133,8 +133,9 @@ export default function Login() {
                         className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Register</a>
                     </p>
                     <GoogleLogin
+                      className='flex justify-center '
                       clientId={clientId}
-                      buttonText=""
+                      buttonText="Login with google"
                       onSuccess={onSuccess}
                       onFailure={onFailure}
                       cookiePolicy={'single_host_origin'}
