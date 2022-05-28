34bff79389da949eeee318ef61b9a4cd50abf4aa
Balkissty <ogundotunbalkiss@gmail.com>
2022-05-28 18:52:26 +0100

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,74 @@
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-    return(
-        <div >
-            <div className="bg-black text-white">
-                <nav className="flex justify-between max-w-7xl mx-auto py-6">
-                    <div className="text-md ">
-                        <ul>
-                        <Link to="/">MyLocale</Link>
-                        </ul>
-                    </div>
-                    <div>
-                        <ul className="flex justify-between space-x-8">
-                            <Link to="/">Home</Link>
-                            <Link to="/users">Users</Link>
-                            <Link to="/Profile">My Profile</Link>
-                            <Link to="/login">Logout</Link>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+export default function Navbar({ fixed }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+
+
+  return (
+    <>
+      <nav className="relative text-md flex flex-wrap items-center justify-between px-2 py-3 bg-black text-white mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#pablo"
+            >
+              <Link to="/">MyLocale</Link>
+            </a>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-md text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <Link to="/">Home</Link>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-md text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <Link to="/users">Users</Link>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-md text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <Link to="/Profile">My Profile</Link>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-md text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <Link to="/login">Logout</Link>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </>
+  );
 }
